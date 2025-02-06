21700796fa3b09f1409b9c8d03082f6b392c6d7a
Naman <namanshrm89@gmail.com>
2025-02-06 21:38:02 +0530

    Learning Java

--- src/ex_02_Logic_Building/IfElseDivisibleby5and11.java
+++ src/ex_02_Logic_Building/IfElseDivisibleby5and11.java
@@ -1,9 +1,17 @@
 package ex_02_Logic_Building;
 
+import java.util.Scanner;
+
 public class IfElseDivisibleby5and11 {
     public static void main(String[] args) {
-        int number = 95;
-        if(number%5==0){
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter a Number");
+        int number = scanner.nextInt();
+        if(number%5==0&&number%11==0){
+            System.out.println("Number is divisible by 5 and 11 both");
+        }
+        else if(number%5==0){
             System.out.println("Number is divisible by 5");
         }
         else if (number%11==0) {
